d17bf9a89b3474843809dcf7c19826b2df6330c8
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-31 20:22:09 +0330

    feat: add Swiper modules

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const Carousel = ({ slides, ...props }) => {
     return (
-        <Swiper {...props}>
+        <Swiper modules={[Pagination]} {...props}>
             {slides.map((slideContent, index) => (
                 <SwiperSlide key={index}>{slideContent}</SwiperSlide>
             ))}
