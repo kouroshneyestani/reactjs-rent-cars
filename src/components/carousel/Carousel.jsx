54904d478c069a93d31913a22bd172a8dcc19bcf
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-31 20:53:46 +0330

    refactor: update & improve swiper pagination styles

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import swiper styles
 import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import "../../assets/plugins/css/swiper.css";
 
 const Carousel = ({ slides, ...props }) => {
     return (
